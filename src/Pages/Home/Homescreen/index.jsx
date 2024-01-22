5126e5f4f00dc856d6dcfb77353dd6b96de51c6e
Remi Lavigne <remilavigne.pro@gmail.com>
2024-01-22 10:51:34 +0100

    footer updated

--- src/Pages/Home/Homescreen/index.jsx
+++ src/Pages/Home/Homescreen/index.jsx
@@ -3,8 +3,7 @@ import ContactMe from "../ContactMe";
 import Footer from "../Footer";
 import HeroSection from "../HeroSection";
 import MyPortfolio from "../MyPortfolio";
-import MySkills from "../MySkills";
-import Testimonial from "../Testimonials";
+import MySkills from "../MySkills"
 
 export default function Home() {
   return (
