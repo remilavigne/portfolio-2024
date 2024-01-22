430c617331cf2c1a8418b323adba253bc19703d7
Remi Lavigne <remilavigne.pro@gmail.com>
2024-01-22 16:09:35 +0100

    Last word section created, need improvement on the global layout

---
+++ src/Pages/Home/LastWord.jsx
@@ -0,0 +1,10 @@
+function LastWord() {
+  return (
+    <div className="LastWord--container--section">
+      <h3 className="LastWord--content--text">Looking forward to grow as a developer and to join a team of talented and passionnate people!</h3>
+      <h1 className="LastWord--content--h1">Ready to bring your projects to life? Let's collaborate and create something exceptional. Hire me, and let's make a lasting impact together!</h1>
+    </div>
+
+  );
+}
+export default LastWord;
