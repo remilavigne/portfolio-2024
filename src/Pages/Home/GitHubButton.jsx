8220b3d83510fbad0ddeb0b6c61df58a15e841b9
Remi Lavigne <remilavigne.pro@gmail.com>
2024-01-23 09:13:17 +0100

    buttons updted in the hero section

---
+++ src/Pages/Home/GitHubButton.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const GitHubButton = () => {
+  return (
+    <a
+      href="https://github.com/remilavigne"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      <button className="">My GitHub</button>
+    </a>
+  );
+};
+
+export default GitHubButton;
