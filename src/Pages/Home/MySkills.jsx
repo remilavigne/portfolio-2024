6dc970d43a0d2738e3e867962bc69d39bde98dab
Remi Lavigne <remilavigne.pro@gmail.com>
2024-01-24 09:36:54 +0100

    New skills section added, need to work on the css

--- src/Pages/Home/MySkills.jsx
+++ src/Pages/Home/MySkills.jsx
@@ -20,6 +20,35 @@ export default function MySkills() {
           </div>
         ))}
       </div>
+      <div className="tada">
+        <div>
+          <h3>Developer</h3>
+          <ul>
+            <li>HTML</li>
+            <li>CSS</li>
+            <li>Javascript</li>
+            <li>Vue.js</li>
+            <li>React.js</li>
+            <li>Bootstrap</li>
+            <li>Tailwind</li>
+            <li>Ruby</li>
+            <li>Ruby on Rails</li>
+            <li>SQL</li>
+          </ul>
+        </div>
+          <div>
+          <h3>UX/UI Design</h3>
+          <ul>
+            <li>Design Thinking</li>
+            <li>UX research</li>
+            <li>Surveys and interviews</li>
+            <li>User testing</li>
+            <li>Ideation workshops</li>
+            <li>Wireframe & prototype</li>
+            <li>Design system</li>
+          </ul>
+        </div>
+      </div>
     </section>
   );
 }
