3b6bc7c97fb1679af26bcf1672c631fb26cc7204
Remi Lavigne <remilavigne.pro@gmail.com>
2024-01-29 13:35:34 +0100

    last word updzted with new text and color, need to be responsive

--- src/Pages/Home/LastWord.jsx
+++ src/Pages/Home/LastWord.jsx
@@ -1,8 +1,8 @@
 function LastWord() {
   return (
     <div className="LastWord--container--section">
-      <h3 className="LastWord--content--text">Looking forward to grow as a developer and to join a team of talented and passionnate people!</h3>
-      <h1 className="LastWord--content--h1">Ready to bring your projects to life? Let's collaborate and create something exceptional. Hire me, and let's make a lasting impact together!</h1>
+      <h3 className="LastWord--content--text">Let's connect and explore how my skills can be an asset to your team.</h3>
+      <h1 className="LastWord--content--h1">I am eager to collaborate, innovate, and build extraordinary digital experiences together ✌️</h1>
     </div>
 
   );
