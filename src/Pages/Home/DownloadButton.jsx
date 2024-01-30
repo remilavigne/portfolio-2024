05ac23a1500466ee90524227263df2999a062332
Remi Lavigne <remilavigne.pro@gmail.com>
2024-01-30 23:35:05 +0100

    css added on some section

--- src/Pages/Home/DownloadButton.jsx
+++ src/Pages/Home/DownloadButton.jsx
@@ -5,22 +5,17 @@ import { faDownload} from '@fortawesome/free-solid-svg-icons'
 
 const DownloadButton = () => {
   const handleDownload = () => {
-    // Assuming your PDF file is in the public folder
     const pdfUrl = process.env.PUBLIC_URL + '/CV_Remi_Lavigne_EN.pdf';
 
-    // Create an invisible link element
     const link = document.createElement('a');
     link.href = pdfUrl;
     link.download = 'CV_Remi_Lavigne_EN.pdf';
-    link.target = '_blank'; // Open in a new tab if you want
+    link.target = '_blank';
 
-    // Append the link to the body
     document.body.appendChild(link);
 
-    // Trigger the click event to start the download
     link.click();
 
-    // Remove the link from the body after the download
     document.body.removeChild(link);
   };
 
