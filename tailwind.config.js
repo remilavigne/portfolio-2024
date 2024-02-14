894e7d51a4d427f61312d8434c76890d3d756eb7
Remi Lavigne <remilavigne.pro@gmail.com>
2024-02-14 15:50:33 +0100

    taiwind and some css updated

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/Pages/Home/Homescreen/index.jsx",
+    "./src/Pages/Home/HeroSection.jsx",
+    "./src/Pages/Home/AboutMe.jsx",
+    "./src/Pages/Home/ContactMe.jsx",
+    "./src/Pages/Home/DownloadButton.jsx",
+    "./src/Pages/Home/Footer.jsx",
+    "./src/Pages/Home/MyPhilosophy.jsx",
+    "./src/Pages/Home/MyPortfolio.jsx",
+    "./src/Pages/Home/MySkills.jsx",
+    "./src/Pages/Home/Navbar.jsx",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
